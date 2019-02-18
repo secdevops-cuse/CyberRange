4d7266e1322285183c4d3ff1a7f4f26e25c92377
cappetta <thomas.cappetta@gmail.com>
2019-02-18 12:47:36 -0500

    wip: testing aws deployment trigger

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "${var.region}"
 //  shared_credentials_file = "/Users/<name>/.aws/credentials"
-  profile                 = "circleci"
+//  profile                 = "circleci"
 
 }
 
