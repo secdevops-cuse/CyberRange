c975727ff5932d4ae6b60f52d5d5d8a16856c487
cappetta <thomas.cappetta@gmail.com>
2019-02-18 22:49:00 -0500

    reducing kali ports

--- terraform/modules/infrastructure/modules/secdevops/security_groups.tf
+++ terraform/modules/infrastructure/modules/secdevops/security_groups.tf
@@ -75,24 +75,13 @@ resource "aws_security_group" "kali" {
   description = "kali"
   vpc_id      = "${var.vpc-id}"
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   # terminal/ssh rule
   ingress {
     from_port = 22
@@ -101,6 +90,13 @@ resource "aws_security_group" "kali" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # inbound rules
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    security_groups = ["${aws_security_group.webgoat.id}"]
+  }
 
   tags {
     Name = "kali-${count.index}"
