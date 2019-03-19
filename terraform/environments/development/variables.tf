43a51e27ba6a66c4c4739f6bbf7e40cbf67b7d3f
cappetta <thomas.cappetta@gmail.com>
2019-03-18 23:17:20 -0400

    adding a bunch of little fixes to the assets

--- terraform/environments/development/variables.tf
+++ terraform/environments/development/variables.tf
@@ -1,3 +1,11 @@
-variable "environment" {default = "development"}
-variable "region" {default = "us-east-1"}
-variable "company" {default = "acs-dev"}
+variable "environment" {
+  default = "development"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "company" {
+  default = "acs-dev"
+}
