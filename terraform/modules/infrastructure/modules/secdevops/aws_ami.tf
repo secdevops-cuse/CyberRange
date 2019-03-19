2758fe5d7b490543aaabc56ed3d627df78a24538
cappetta <thomas.cappetta@gmail.com>
2019-03-18 23:17:20 -0400

    adding a bunch more assets

--- terraform/modules/infrastructure/modules/secdevops/aws_ami.tf
+++ terraform/modules/infrastructure/modules/secdevops/aws_ami.tf
@@ -30,15 +30,14 @@ data "aws_ami" "kali" {
   owners = ["679593333241"]
 }
 
-
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
   owners      = ["amazon"]
+
   filter {
     name   = "owner-alias"
     values = ["amazon"]
   }
-
 
   filter {
     name   = "name"
