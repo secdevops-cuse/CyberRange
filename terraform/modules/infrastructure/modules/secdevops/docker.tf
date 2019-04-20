9f89087d4170b76552c20a0726d69ddd939906c7
cappetta <thomas.cappetta@gmail.com>
2019-04-19 23:48:48 -0400

    minor changes

--- terraform/modules/infrastructure/modules/secdevops/docker.tf
+++ terraform/modules/infrastructure/modules/secdevops/docker.tf
@@ -12,18 +12,13 @@ resource "aws_instance" "docker" {
 
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "${var.instance_type_docker}"
-
   subnet_id              = "${element(local.docker_subnets_ids, count.index)}"
   vpc_security_group_ids = ["${aws_security_group.webgoat.id}"]
-
   key_name = "${aws_key_pair.circleci_key.key_name}"
-
   user_data = "${file("../../modules/infrastructure/cloud-init/docker.targets.yml")}"
-
   root_block_device {
     delete_on_termination = true
   }
-
   tags = {
     Name        = "docker-web-apps-${count.index}"
     Environment = "${var.environment}"
