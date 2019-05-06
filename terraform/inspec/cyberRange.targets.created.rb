589d580bbcd1e3f13ed75ce3c6a795f0b6ccdebc
cappetta <thomas.cappetta@gmail.com>
2019-05-06 00:23:53 -0400

    updating inspec test

--- terraform/inspec/cyberRange.targets.created.rb
+++ terraform/inspec/cyberRange.targets.created.rb
@@ -3,32 +3,36 @@
 describe aws_subnets() do
   it { should exist }
   its('cidr_blocks') {should_not cmp []} #{ should be_in ['10.0.1.0/24','10.0.2.0/24','10.0.3.0/24','10.0.4.0/24', '10.0.5.0/24'] }
-  its('cidr_blocks') { should cmp ['10.0.1.0/24','10.0.2.0/24','10.0.3.0/24','10.0.4.0/24','10.0.5.0/24'] }
+  its('cidr_blocks') { should include '10.0.1.0/24'}
+  its('cidr_blocks') { should include '10.0.2.0/24'}
+  its('cidr_blocks') { should include '10.0.3.0/24'}
+  its('cidr_blocks') { should include '10.0.4.0/24'}
+  its('cidr_blocks') { should include '10.0.5.0/24'}
 end
 
-describe aws_ec2_instance(name: "CyberRange_win2012_RTM-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_hackinos-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_Stapler-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_docker-web-apps-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_myhouse7-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_flarevm-win7-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_nessus-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_kali-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_r7-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_Skytower-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_win2008-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_win2003-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_win2019-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_win7-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_MS3-win2k12-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_defender_win2k10-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_ms3_nix-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_MrRobot-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_win8-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_bulldog-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_kali-linux") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_Vulnos-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_win2012-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_commando-pT16-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_fristileaks-0") do it { should exist }; end
-describe aws_ec2_instance(name: "CyberRange_Sickos-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-win2012_RTM-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-hackinos-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-Stapler-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-docker-web-apps-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-myhouse7-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-flarevm-win7-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-nessus-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-kali-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-r7-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-Skytower-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-win2008-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-win2003-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-win2019-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-win7-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-MS3-win2k12-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-win2k10-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-ms3_nix-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-MrRobot-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-win8-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-bulldog-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-kali-linux") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-Vulnos-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-win2012-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-commando-pT16-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-fristileaks-0") do it { should exist }; end
+describe aws_ec2_instance(name: "CyberRange-Sickos-0") do it { should exist }; end
