2737da3017286a0d47ceb4f366718003f66018bd
cappetta <thomas.cappetta@gmail.com>
2019-05-09 23:25:46 -0400

    removing dup rule

--- terraform/modules/infrastructure/modules/secdevops/security_groups_rules.tf
+++ terraform/modules/infrastructure/modules/secdevops/security_groups_rules.tf
@@ -18,15 +18,6 @@ resource "aws_security_group_rule" "allow_all_between_kali_and_webgoat" {
   security_group_id = "${aws_security_group.kali.id}"
 }
 
-resource "aws_security_group_rule" "allow_rdp_from_kali_to_walware" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 3389
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.kali.id}"
-}
-
 resource "aws_security_group_rule" "allow_malware_to_malware" {
   type              = "ingress"
   from_port         = 0
