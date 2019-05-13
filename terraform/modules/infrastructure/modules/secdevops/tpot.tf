30f7bece5ca31fc31fcfaa7434baf6148bf867d9
cappetta <thomas.cappetta@gmail.com>
2019-05-13 16:13:22 -0400

    update ami and add cloud-init for tpot

--- terraform/modules/infrastructure/modules/secdevops/tpot.tf
+++ terraform/modules/infrastructure/modules/secdevops/tpot.tf
@@ -8,16 +8,12 @@ locals {
 resource "aws_instance" "tpot" {
   count = "${var.tpot_ct}"
 
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = "${data.aws_ami.cuckoo-host.id}"
   instance_type = "${var.instance_type_tpot}"
-
   subnet_id = "${element(local.subnets_ids, count.index)}"
-
   vpc_security_group_ids = ["${aws_security_group.tpot.id}"]
-
   key_name = "${aws_key_pair.circleci_key.key_name}"
-
-  user_data = "${file("${var.tpot_user_data}")}"
+  user_data = "${file("../../modules/infrastructure/cloud-init/tpot.setup.yml")}"
 
   root_block_device {
     volume_type = "gp2"
