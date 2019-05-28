f5de356f9c1e5e1d6c31d885d8cf51baa6ab12c5
cappetta <thomas.cappetta@gmail.com>
2019-05-28 01:23:27 -0400

    eliminating unnecessary variable

--- terraform/environments/development/variables.tf
+++ terraform/environments/development/variables.tf
@@ -1,11 +1,2 @@
-variable "environment" {
-  default = "development"
-}
-
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "company" {
-  default = "acs-dev"
-}
+variable "environment" {default = "development"}
+variable "region"      {default = "us-east-1"}
--- terraform/environments/production/variables.tf
+++ terraform/environments/production/variables.tf
@@ -1,3 +1,2 @@
 variable "environment" {default = "production"}
-variable "region" {default = "us-east-1"}
-variable "company" {default = "acs-prod"}
+variable "region"      {default = "us-east-1"}
