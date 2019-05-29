73b21755d4e305b4cd095f853a2aa442d7064156
cappetta <thomas.cappetta@gmail.com>
2019-05-29 14:04:13 -0400

    need to add main to the subnet / sg refactor

--- terraform/modules/infrastructure/main.tf
+++ terraform/modules/infrastructure/main.tf
@@ -11,8 +11,8 @@ module "secdevops" {
   vpc-id         = "${module.network.vpc-id}"
   vpc-cidr-block = "${module.network.vpc-cidr-block}"
 
-  public-a-subnet-id = "${module.network.public-a-subnet-id}"
-  public-b-subnet-id = "${module.network.public-b-subnet-id}"
+  target_subnet_id= "${module.network.target_subnet_id}"
+  attacker_subnet_id = "${module.network.attacker_subnet_id}"
 
   private-a-subnet-id = "${module.network.private-a-subnet-id}"
   private-b-subnet-id = "${module.network.private-b-subnet-id}"
