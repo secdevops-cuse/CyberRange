f342925eca80e038b9881ae9e7620ed559d41b70
cappetta <thomas.cappetta@gmail.com>
2019-05-29 22:14:09 -0400

    eliminating subnet conflict

--- terraform/modules/infrastructure/modules/network/subnets.tf
+++ terraform/modules/infrastructure/modules/network/subnets.tf
@@ -51,7 +51,7 @@ resource "aws_subnet" "malware_subnet" {
 
 resource "aws_subnet" "honeypot_subnet" {
   vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "10.0.3.0/24"
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "${data.aws_availability_zones.available.names[1]}"
   map_public_ip_on_launch = true
 
