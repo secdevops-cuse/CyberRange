4864cf226bbf486ca99141ebb574a44769b42ada
cappetta <thomas.cappetta@gmail.com>
2019-08-24 18:34:40 -0400

    v2: updating tpot sg rules

--- terraform/modules/infrastructure/assets/secdevops/security_groups.tf
+++ terraform/modules/infrastructure/assets/secdevops/security_groups.tf
@@ -5,40 +5,34 @@ resource "aws_security_group" "tpot" {
 
   ingress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    to_port     = 64000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port   = 0
+    to_port     = 64000
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 64294
+    to_port     = 64294
+    protocol    = "tcp"
+    cidr_blocks = ["${var.ip_list}"]
+  }
   ingress {
     from_port   = 64295
     to_port     = 64295
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.ip_list}"]
   }
-
   ingress {
     from_port   = 64297
     to_port     = 64297
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.ip_list}"]
   }
-
-  ingress {
-    from_port   = 64298
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all traffic from the private subnet
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["10.0.1.0/24"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
