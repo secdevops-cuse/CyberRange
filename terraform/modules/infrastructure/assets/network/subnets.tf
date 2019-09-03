8563b3d23f1bcde2f449af1f11a04d4be2d4dc77
cappetta <thomas.cappetta@gmail.com>
2019-09-03 08:57:39 -0400

    v2: changing subnet to class c subnet

--- terraform/modules/infrastructure/assets/network/subnets.tf
+++ terraform/modules/infrastructure/assets/network/subnets.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "attacker_subnet" {
 resource "aws_subnet" "target_subnet" {
   vpc_id                  = "${aws_vpc.main.id}"
 //  cidr_block              = "10.0.2.0/24"
-  cidr_block        = "${var.cidr}"
+  cidr_block        = "192.168.39.0/24"
   availability_zone       = "${data.aws_availability_zones.available.names[0]}"
 //  map_public_ip_on_launch = true
 
@@ -34,7 +34,7 @@ resource "aws_subnet" "target_subnet" {
 resource "aws_subnet" "malware_subnet" {
   vpc_id            = "${aws_vpc.main.id}"
 //  cidr_block        = "10.0.3.0/24"
-  cidr_block        = "${var.cidr}"
+  cidr_block        = "192.168.40.0/24"
   availability_zone = "${data.aws_availability_zones.available.names[1]}"
   map_public_ip_on_launch = true
 
@@ -51,7 +51,7 @@ resource "aws_subnet" "malware_subnet" {
 resource "aws_subnet" "honeypot_subnet" {
   vpc_id            = "${aws_vpc.main.id}"
 //  cidr_block        = "10.0.4.0/24"
-  cidr_block        = "${var.cidr}"
+  cidr_block        = "192.168.41.0/24"
   availability_zone = "${data.aws_availability_zones.available.names[1]}"
   map_public_ip_on_launch = true
 
@@ -67,7 +67,8 @@ resource "aws_subnet" "honeypot_subnet" {
 
 resource "aws_subnet" "private-a" {
   vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "10.0.253.0/24"
+//  cidr_block        = "10.0.253.0/24"
+  cidr_block        = "192.168.42.0/24"
   availability_zone = "${data.aws_availability_zones.available.names[0]}"
 
   tags = {
@@ -82,7 +83,8 @@ resource "aws_subnet" "private-a" {
 
 resource "aws_subnet" "private-b" {
   vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "10.0.252.0/24"
+  cidr_block        = "192.168.43.0/24"
+//  cidr_block        = "10.0.252.0/24"
   availability_zone = "${data.aws_availability_zones.available.names[1]}"
 
   tags = {
