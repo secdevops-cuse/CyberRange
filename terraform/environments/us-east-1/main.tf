8bbe02eb5222ba7d15be950cd7dbcbec97c3b2f9
cappetta <thomas.cappetta@gmail.com>
2020-06-07 21:35:23 -0400

    reverting us-east-1 main.tf back to terraform v0.11.14 standards

--- terraform/environments/us-east-1/main.tf
+++ terraform/environments/us-east-1/main.tf
@@ -1,25 +1,15 @@
 provider "aws" {
-  region                  = var.region
-  shared_credentials_file = pathexpand("~/.aws/credentials")
-  profile = "us-east-1"
+  region                  = "${var.region}"
+  shared_credentials_file = "${pathexpand("~/.aws/credentials")}"
 }
 
 module "staging-state" {
   source      = "../../modules/state"
-  environment = var.environment
+  environment = "${var.environment}"
 }
 
-//terraform {
-//  backend "s3" {
-//    bucket  = "secdevops-cuse"
-//    key     = "virginia/secdevops-cuse-dev.tfstate"
-//    region  = "us-east-1"
-//    encrypt = true
-//  }
-//}
 
-module "range-infra" {
+module "staging-infrastructure" {
   source      = "../../modules/infrastructure"
-  environment = var.environment
-  cidr = var.cidr
+  environment = "${var.environment}"
 }
